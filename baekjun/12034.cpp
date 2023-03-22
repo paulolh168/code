062234d5d2e3331e6811214695057081a066adca
paulolh168 <paulolh168@konkuk.ac.kr>
2023-03-22 10:00:43 +0900

    Update 12034.cpp

--- baekjun/12034.cpp
+++ baekjun/12034.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 int  plus[200];
 int main() {
-	int num;
+	int num,max[100];
 	std::cin >> num;
 	for (int i = 0;i < num;i++) {
 		int many,max[100];
 		std::cin >> many;
-		for (int j = 0;j < 2*many;j++)std::cin >> plus[i]; 
-		
+		for (int j = 0;j < 2*many;j++) std::cin >> plus[i]; 
+		for (int j = 0;j < 2 * many;j++){
+			if (plus[2 * many - 1] / 4 == plus[j]) {
+				break;
+			}
 	}
-}
