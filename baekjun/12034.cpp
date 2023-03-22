87867550aa0438ebc54814dc416cbf9cdcd27568
paulolh168 <128040224+paulolh168@users.noreply.github.com>
2023-03-22 09:53:45 +0900

    Create 12034.cpp

---
+++ baekjun/12034.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int  plus[200];
+int main() {
+	int num;
+	std::cin >> num;
+	for (int i = 0;i < num;i++) {
+		int many,max[100];
+		std::cin >> many;
+		for (int j = 0;j < 2*many;j++)std::cin >> plus[i]; 
+		
+	}
+}
